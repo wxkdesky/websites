9e6261bc8a19d5cd37dd0b28e52d37b5d51cbb3a
wxkdesky <wxkdesky@hotmail.com>
2015-06-04 14:44:53 +0800

    实现了js的正则，当然啦，C#也可以了

---
+++ mythings/reg.js
@@ -0,0 +1,17 @@
+﻿var reg = new RegExp("([^;]+);","ig");
+var words = "hello;world;and;hello;to you;";
+var i = 0;
+var y=new Array();
+var xx;// = reg.exec(words);
+while ((xx = reg.exec(words)) != null) {
+    y[i] = xx[1];
+    i = i + 1;
+}
+for (i = 0; i < y.length ; i++) {
+ document.getElementById("test").innerHTML+=y[i]+"<br />"
+}
+//var xx = words.match(reg);
+//for (var i = 0; i < xx.length ; i++) {
+    // document.write("<p>"+xx[i] + "<br /></p>");
+   // document.getElementById("test").innerHTML+=xx[i]+"<br />"
+//}
